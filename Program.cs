ea48bb6c17d87e451488836320a80db959d57205
sap1ks <sap1kcsgo@gmail.com>
2024-11-18 17:54:33 +0200

    add1

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практикум9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHello();
+            SayBye();
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+        }
+        static void SayHello()
+        {
+            Console.WriteLine("Привет");
+        }
+        static void SayBye()
+        {
+            Console.WriteLine("Пока");
+        }
+
+    }
+
+}        
+
+        
+        
+            
+        
+
+
+   
+
