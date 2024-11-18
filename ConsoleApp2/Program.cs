64377b74b210b8d2452a8699231853800b08a0e8
sap1ks <sap1kcsgo@gmail.com>
2024-11-18 18:08:35 +0200

    add3

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"b={Func(a)}");
+            Console.ReadKey();
+        }
+        static double Func(double a)
+        {
+            return 12 * Math.Pow(a, 2) + 7 * a - 6;
+        }
+              
+    }
+}
